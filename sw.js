8052bf0d874029750c835dee47f567a76f1424f8
Liliia <lilia.sainchuk@gmail.com>
2026-09-23 13:55:30 +0200

    Show all idea categories (wrap chips)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'someday-v2';
+const CACHE = 'someday-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
