9869b6f2ba4d0a4487a0f36b33555a7b20d98ce0
Liliia <lilia.sainchuk@gmail.com>
2026-09-23 11:50:48 +0000

    Fix bottom content hidden behind the menu bar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'someday-v1';
+const CACHE = 'someday-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
